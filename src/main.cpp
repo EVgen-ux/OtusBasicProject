ee4aacecd2a8003e41b7c8f1962c15eab9e10123
Evg <velichko-ev@mail.ru>
2025-03-08 15:41:48 +0300

    обернул main в try/catch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "Game.h"
+#include <iostream>
+#include <stdexcept>
 
 int main() {
-  Game game;
-  game.run();
-  return 0;
+  try {
+    Game game;
+    game.run();
+} catch (const std::exception& e) {
+    std::cerr << "Произошла ошибка: " << e.what() << std::endl;
+    return 1;
+}
+
+return 0;
 }
